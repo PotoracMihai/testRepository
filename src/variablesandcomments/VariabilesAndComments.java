408d5cd5ec59460f2d1578f3a50a67aed2450f8e
Mihai <potoracmihai74@gmail.com>
2024-10-01 10:32:32 +0300

    Added the Variables and Comments Task

---
+++ src/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,24 @@
+package variablesandcomments;
+
+public class VariabilesAndComments {
+    public static void main(String[] args) {
+
+        //Declare the primitive data type variables
+        boolean falseBooleanVariable = true;
+        boolean trueBoolleanVariable = false;
+        char firstCharFromAscii = 'a';
+        char lastCharFromAscii = 'z';
+        byte lowerLimitOfByteDataType = -128;
+        byte upperLimitOfByteDataType = 127;
+        short lowerLimitOfShortDataType = -32768;
+        short upperLimitOfShortDataType = 32767;
+        int lowerLimitOfIntDataType = -2147483648;
+        int upperLimitOfIntDataType = 2147483647;
+        long lowerLimitOfLongDataType = -9223372036854775808L;
+        long upperLimitOfLongDataType = 9223372036854775807L;
+        float lowerLimitOfFloatDataType = -3.4028235E38f;
+        float upperLimitOfFloatDataType = 3.4028235E38f;
+        double lowerLimitOfDoubleDataType = -1.7976931348623157E308;
+        double upperLimitOfDoubleDataType = 1.7976931348623157E308;
+    }
+}
